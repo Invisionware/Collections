99d016ec610c8ec2e155248e79297bca142b983c
Shawn Anderson <sanderson@eye-catcher.com>
2017-06-03 14:29:14 -0400

    Reorganized into new folder structure

---
+++ src/Invisionware.Collections/DictionaryExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Invisionware.Collections
+{
+	/// <summary>
+	/// Class Extensions.
+	/// </summary>
+	public static class DictionaryExtensions
+	{
+		/// <summary>
+		/// Renames the key.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the t key.</typeparam>
+		/// <typeparam name="TValue">The type of the t value.</typeparam>
+		/// <param name="source">The dictionary.</param>
+		/// <param name="oldKey">The old key.</param>
+		/// <param name="newKey">The new key.</param>
+		/// <returns><c>true</c> if cannot get the value, <c>false</c> otherwise true.</returns>
+		public static bool RenameKey<TKey, TValue>(this IDictionary<TKey, TValue> source,
+			TKey oldKey, TKey newKey)
+		{
+			TValue value;
+			if (!source.TryGetValue(oldKey, out value))
+				return false;
+
+			source.Remove(oldKey);  // do not change order
+			source[newKey] = value;  // or source.Add(newKey, value) depending on ur comfort
+			return true;
+		}
+
+		/// <summary>
+		/// Renames the key.
+		/// </summary>
+		/// <param name="source">The dictionary.</param>
+		/// <param name="oldKey">The old key.</param>
+		/// <param name="newKey">The new key.</param>
+		/// <returns><c>true</c> if cannot get value, <c>false</c> otherwise true.</returns>
+		public static bool RenameKey(this IDictionary<string, string> source, string oldKey, string newKey)
+		{
+			return source.RenameKey<string, string>(oldKey, newKey);
+		}
+	}
+}
---
+++ tests/Invisionware.Collections.Tests/DictionaryExtensions.Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Invisionware.Collections.Tests
+{
+	[TestFixture]
+	public class DictionaryExtensionsTests
+	{
+		private readonly IDictionary<string, string> _dictionaryStringString = new Dictionary<string, string>
+		{
+			{"key1", "value1"},
+			{"key2", "value2"},
+			{"key3", "value3"},
+		};
+
+		private readonly IDictionary<string, int> _dictionaryStringInt = new Dictionary<string, int>()
+		{
+			{"key1", 0},
+			{"key2", 1},
+			{"key3", 2},
+		};
+			
+		[SetUp]
+		public void Initialize()
+		{
+			
+		}
+
+		[Test]
+		public void RenameKeyString()
+		{
+			Assert.IsTrue(_dictionaryStringString.ContainsKey("key1"));
+	
+			_dictionaryStringString.RenameKey("key1", "key1a");
+
+			Assert.IsFalse(_dictionaryStringString.ContainsKey("key1"));
+			Assert.IsTrue(_dictionaryStringString.ContainsKey("key1a"));
+		}
+
+		[Test]
+		public void RenameKeyNonString()
+		{
+			Assert.IsTrue(_dictionaryStringInt.ContainsKey("key1"));
+
+			_dictionaryStringInt.RenameKey("key1", "key1a");
+
+			Assert.IsFalse(_dictionaryStringInt.ContainsKey("key1"));
+			Assert.IsTrue(_dictionaryStringInt.ContainsKey("key1a"));
+		}
+	}
+}
